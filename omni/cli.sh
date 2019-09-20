91250b9e714fdab09543af818711c3144ecd5c4b
apa7 <apa777@yeah.net>
2019-09-20 10:40:12 +0800

    OMNI

---
+++ omni/cli.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+omnicore-cli -rpcport=18335 -rpcconnect=192.168.1.70 -conf=/home/omni/omni.conf $1 $2 $3 $4 $5 $6 $7
---
+++ omni/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+omnicored -conf=/home/omni/omni.conf -regtest -rpcport=18335 -rpcbind=192.168.1.70 -bind=192.168.1.70:18445
